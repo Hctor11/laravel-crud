db5d8c1420be99bf9a4cf223a9ba96a5aa3bd5ea
hector <hectorrrivera1010@gmail.com>
2023-11-29 12:33:22 -0600

    [add] connection to database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,6 +14,9 @@ class UserController extends Controller
             'email' => ['required','email'],
             'password' => ['required','min:8','max:100'],
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        User::create($incomingFields);
 
         return "hola desde el controlador";
     }
