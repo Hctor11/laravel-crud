eec386f61e98f84d23db98f0a883833535f37877
hector <hectorrrivera1010@gmail.com>
2023-11-28 23:30:47 -0600

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <style>
+        html {
+            font-family: Figtree, sans-serif;
+            font-feature-settings: normal
+        }
+    </style>
+</head>
+
+<body>
+    <h1>TESTEO WEBSITE LARAVEL OMG</h1>
+</body>
+
+</html>
